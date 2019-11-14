6818e753c947ddf64f2765ebc784fe5edcc050c8
unknown <296270991@qq.com>
2019-11-14 13:14:14 +0800

    spin lock

---
+++ mutex/spinlock.go
@@ -0,0 +1,30 @@
+package mutex
+
+import "sync/atomic"
+
+//SpinLock desc
+//@struct SpinLick desc: spin lock
+//@member (uint32)
+type SpinLock struct {
+	kernel uint32
+}
+
+//Trylock desc
+//@method Trylock desc: try lock if unlock return false
+//@return (bool)
+func (slf *SpinLock) Trylock() bool {
+	return atomic.CompareAndSwapUint32(&slf.kernel, 0, 1)
+}
+
+//Lock desc
+//@method Lock desc: locking
+func (slf *SpinLock) Lock() {
+	for !atomic.CompareAndSwapUint32(&slf.kernel, 0, 1) {
+	}
+}
+
+//Unlock desc
+//@method Unlock desc: unlocking
+func (slf *SpinLock) Unlock() {
+	atomic.StoreUint32(&slf.kernel, 0)
+}
