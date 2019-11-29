b5de4f4af366b0f5586ed4d6754296e46391638d
296270991@qq.com <296270991@qq.com>
2019-11-29 16:51:04 +0800

    fixed mysql bug

--- dbs/mysql.go
+++ dbs/mysql.go
@@ -175,7 +175,7 @@ func (slf *MySQLValue) ToByte() []byte {
 
 //ToTimeStamp desc
 //@method ToTimeStamp desc: Return  time int64 value
-//@return (int64) a value ss
+//@return (int64) a value
 func (slf *MySQLValue) ToTimeStamp() int64 {
 	v := slf.ToDateTime()
 	if v == nil {
