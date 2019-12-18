745b56c6bf1558ecfabf41c558f3ed1e9ee3b5d6
296270991@qq.com <296270991@qq.com>
2019-12-18 16:43:51 +0800

    append union

--- st/sets/hashset/hashset.go
+++ st/sets/hashset/hashset.go
@@ -11,8 +11,14 @@ type Set struct {
 	items map[interface{}]struct{}
 }
 
+//Initial doc
+//@Summary initial Set
+func (s *Set) Initial() {
+	s.items = make(map[interface{}]struct{})
+}
+
 //Push doc
-//@Method Push @Summary adds the es (one or more) to the set
+//@Summary adds the es (one or more) to the set
 //@Param  (...interface{}) insert elements
 func (s *Set) Push(es ...interface{}) {
 	for _, it := range es {
@@ -21,7 +27,7 @@ func (s *Set) Push(es ...interface{}) {
 }
 
 //PushAll doc
-//@Method PushAll @Summary adds the st(set in element) to the set.
+//@Summary adds the st(set in element) to the set.
 //@Param (*Set) sets
 func (s *Set) PushAll(st *Set) {
 	for _, it := range st.items {
@@ -29,8 +35,41 @@ func (s *Set) PushAll(st *Set) {
 	}
 }
 
+//Union doc
+//@Summary union the es to the set
+func (s *Set) Union(eds ...interface{}) *Set {
+	nset := &Set{}
+	nset.Initial()
+
+	for _, it := range eds {
+		nset.Push(it)
+	}
+
+	for it := range s.items {
+		nset.Push(it)
+	}
+
+	return nset
+}
+
+//UnionAll doc
+//@Summary Returns two set is union
+func (s *Set) UnionAll(st *Set) *Set {
+	nset := &Set{}
+	nset.Initial()
+
+	for it := range st.items {
+		nset.Push(it)
+	}
+
+	for it := range s.items {
+		nset.Push(it)
+	}
+	return nset
+}
+
 //Retain doc
-//@Method Retain @Summary retain the es (one or more) to the set.
+//@Summary retain the es (one or more) to the set.
 //@Param (...interface{}) elements
 func (s *Set) Retain(eds ...interface{}) {
 	vs := make(map[interface{}]struct{})
@@ -44,11 +83,11 @@ func (s *Set) Retain(eds ...interface{}) {
 }
 
 //RetainAll doc
-//@Method RetainAll @Summary retain the st(set in element) to the set.
+//@Summary retain the st(set in element) to the set.
 //@Param (*Set) sets
 func (s *Set) RetainAll(st *Set) {
 	vs := make(map[interface{}]struct{})
-	for _, it := range st.items {
+	for it := range st.items {
 		if v, ok := s.items[it]; ok {
 			vs[it] = v
 		}
@@ -57,7 +96,7 @@ func (s *Set) RetainAll(st *Set) {
 }
 
 //Erase doc
-//@Method Erase @Summary removes the es (one or more) from the set
+//@Summary removes the es (one or more) from the set
 //@Param  (...interface{}) elements
 func (s *Set) Erase(es ...interface{}) {
 	for _, it := range es {
@@ -66,16 +105,16 @@ func (s *Set) Erase(es ...interface{}) {
 }
 
 //EraseAll doc
-//@Method EraseAll @Summary removes this st(set in element) from the set
+//@Summary removes this st(set in element) from the set
 //@Param (*Set) sets
 func (s *Set) EraseAll(st *Set) {
-	for _, it := range st.items {
+	for it := range st.items {
 		delete(s.items, it)
 	}
 }
 
 //Contains doc
-//@Method Contains @Summary check if es (one or more) are present in the set.
+//@Summary check if es (one or more) are present in the set.
 //@Param  (...interface{}) elements
 //@Return (bool)
 func (s *Set) Contains(es ...interface{}) bool {
@@ -88,27 +127,27 @@ func (s *Set) Contains(es ...interface{}) bool {
 }
 
 //Size doc
-//@Method Size @Summary returns number of elements within the set.
+//@@Summary returns number of elements within the set.
 //@Return (int) size
 func (s *Set) Size() int {
 	return len(s.items)
 }
 
 //IsEmpty doc
-//@Method IsEmpty @Summary returns true if set does not contain any elements.
+//@Summary returns true if set does not contain any elements.
 //@Param (bool)
 func (s *Set) IsEmpty() bool {
 	return s.Size() == 0
 }
 
 //Clear doc
-//@Method Clear @Summary clears all values in the set.
+//@Summary clears all values in the set.
 func (s *Set) Clear() {
 	s.items = make(map[interface{}]struct{})
 }
 
 //Values doc
-//@Method Values @Summary returns all items in the set.
+//@Summary returns all items in the set.
 //@Return ([]interface{})
 func (s *Set) Values() []interface{} {
 	vs := make([]interface{}, s.Size())
@@ -121,7 +160,7 @@ func (s *Set) Values() []interface{} {
 }
 
 //String doc
-//@Method String @Summary Returns a string
+//@Summary Returns a string
 //@Return (string)
 func (s *Set) String() string {
 	str := "HashSet\n"
