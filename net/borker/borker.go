381081972fcd56f572fef3038f30a2ceb8da1fc5
yamakiller <296270991@qq.com>
2020-02-13 09:20:16 +0800

    network borker

--- net/borker/borker.go
+++ net/borker/borker.go
@@ -1,8 +1,11 @@
 package borker
 
+import "github.com/yamakiller/magicLibs/net/listener"
+
 //Borker 网络代理服务
 type Borker interface {
 	ListenAndServe(string) error
+	Listener() listener.Listener
 	Serve() error
 	Shutdown()
 }
