b9181c187ca50047050f7aacae6a4c77bc410b20
yamakiller <296270991@qq.com>
2020-02-19 08:47:22 +0800

    增加获取当前毫秒级时间

--- util/time.go
+++ util/time.go
@@ -2,9 +2,14 @@ package util
 
 import "time"
 
-//TimeNowFormat doc
-//@Method TimeNowFormat @Summary get now time and format
+//TimeNowFormat Returns　当前时间字符串
+//@Summary get now time and format
 //@Return (string) YYYY-MM-DD hh:ii:ss
 func TimeNowFormat() string {
 	return time.Now().Format("2006-01-02 15:04:05")
+}
+
+//Timestamp Returns 当前时间毫秒
+func Timestamp() int64 {
+	return time.Now().UnixNano() / 1e6
 }
